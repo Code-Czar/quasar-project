e83767cb748cb91620e396b78d0f3b6a3ef6526c
beniben <beniben@benibens-MacBook-Pro.local>
2024-11-14 17:30:46 +0100

    fix: autoupdater

--- src-electron/electron-main.ts
+++ src-electron/electron-main.ts
@@ -24,17 +24,9 @@ import { exec, spawn } from 'child_process';
 const platform = process.platform || os.platform();
 const isProduction = process.env.NODE_ENV === 'production';
 
-const REPO_OWNER = 'Code-Czar';
-const REPO_NAME = 'quasar-project';
-const ASAR_DOWNLOAD_URL = `https://github.com/${REPO_OWNER}/${REPO_NAME}/releases/latest/download/app-asar.zip`;
-const LATEST_YML_URL = `https://github.com/${REPO_OWNER}/${REPO_NAME}/releases/latest/download/latest.yml`;
 const appUrl = 'http://localhost:9000';
 
-const CURRENT_VERSION = app.getVersion();
 const resourcesPath = app.getPath('userData');
-const asarPath = path.join(resourcesPath, 'app.asar');
-const zipPath = path.join(resourcesPath, 'app-asar.zip');
-const latestYmlPath = path.join(resourcesPath, 'latest.yml');
 const logFile = fs.createWriteStream(`${resourcesPath}/app.log`, {
   flags: 'a',
 });
@@ -110,40 +102,6 @@ const initWebSocket = () => {
   });
 };
 
-async function setPermissions(filePath: string, permissions: number) {
-  try {
-    await fs.promises.chmod(filePath, permissions);
-    console.log(
-      `Set permissions for "${filePath}" to ${permissions.toString(8)}`,
-    );
-  } catch (error) {
-    log.error(`Error setting permissions for "${filePath}":`, error);
-  }
-}
-
-async function getLocalVersion(): Promise<string | undefined> {
-  try {
-    console.log(`Local path: ${latestYmlPath}`);
-    if (fs.existsSync(latestYmlPath)) {
-      const ymlContent = fs.readFileSync(latestYmlPath, 'utf-8');
-      const parsedYml = yaml.load(ymlContent);
-      console.log(`parsed yml: ${parsedYml}`);
-      // @ts-expect-error ignoring
-      if (parsedYml && parsedYml.version) {
-        // @ts-expect-error ignoring
-        console.log(`local version: ${parsedYml.version.replace(/^v/, '')}`);
-        // @ts-expect-error ignoring
-        return parsedYml.version.replace(/^v/, '');
-      }
-    }
-  } catch (error) {
-    log.error('Error reading local latest.yml:', error);
-    return '';
-  }
-  console.log(`local version: ${CURRENT_VERSION}`);
-  return CURRENT_VERSION;
-}
-
 // Helper function to handle worker spawning
 function spawnWorker(scriptPath: string, args: any = {}): Promise<any> {
   return new Promise((resolve, reject) => {
@@ -182,123 +140,6 @@ function spawnWorker(scriptPath: string, args: any = {}): Promise<any> {
       }
     });
   });
-}
-
-async function checkForAppUpdate() {
-  try {
-    if (!app.isPackaged) {
-      console.log('Development mode detected, skipping update check.');
-      return;
-    }
-    const localVersion = await getLocalVersion();
-    console.log(`Local version 1: ${localVersion}`);
-    await delay(5000);
-
-    const response = await axios.get(
-      `https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/releases/latest`,
-    );
-    const latestVersion = response.data.tag_name.replace(/^v/, '');
-
-    console.log(
-      `Local version: ${localVersion}, Latest version: ${latestVersion}`,
-    );
-    await delay(5000);
-
-    if (latestVersion === localVersion) {
-      console.log(
-        'Local version matches the latest version. No update needed.',
-      );
-      return;
-    }
-
-    console.log(`New version detected. Prompting user for update...`);
-    const dialogOpts: Electron.MessageBoxOptions = {
-      type: 'info',
-      buttons: ['Download and Install', 'Later'],
-      title: 'Update Available',
-      message: `A new version (${latestVersion}) is available. Would you like to download and install it?`,
-    };
-
-    const { response: userResponse } = await dialog.showMessageBox(dialogOpts);
-    if (userResponse === 0) {
-      await downloadAndInstallAsar(latestVersion);
-    }
-  } catch (error) {
-    log.error('Error checking for updates:', error);
-  }
-}
-
-async function downloadAndInstallAsar(latestVersion: string) {
-  try {
-    console.log(`Starting download of app-asar.zip to "${zipPath}"`);
-    const response = await axios({
-      url: ASAR_DOWNLOAD_URL,
-      method: 'GET',
-      responseType: 'stream',
-    });
-    const writer = fs.createWriteStream(zipPath);
-    response.data.pipe(writer);
-
-    await new Promise<void>((resolve, reject) => {
-      writer.on('finish', resolve);
-      writer.on('error', reject);
-    });
-    console.log(`Downloaded app-asar.zip to "${zipPath}"`);
-
-    console.log(`Starting download of latest.yml to "${latestYmlPath}"`);
-    const ymlResponse = await axios({
-      url: LATEST_YML_URL,
-      method: 'GET',
-      responseType: 'stream',
-    });
-    const ymlWriter = fs.createWriteStream(latestYmlPath);
-    ymlResponse.data.pipe(ymlWriter);
-
-    await new Promise<void>((resolve, reject) => {
-      ymlWriter.on('finish', resolve);
-      ymlWriter.on('error', reject);
-    });
-    console.log(`Downloaded latest.yml to "${latestYmlPath}"`);
-
-    await setPermissions(zipPath, 0o777);
-    await setPermissions(latestYmlPath, 0o644);
-
-    await delay(2000);
-
-    process.noAsar = true;
-
-    console.log(`Starting extraction of "${zipPath}" to "${resourcesPath}"`);
-    const zip = new AdmZip(zipPath);
-    zip.extractAllTo(resourcesPath, true);
-
-    const extractedFiles = fs.readdirSync(resourcesPath);
-    console.log(
-      `Extracted files in resources folder: ${extractedFiles.join(', ')}`,
-    );
-
-    if (!fs.existsSync(asarPath)) {
-      throw new Error(
-        `Extracted file "app.asar" not found in "${resourcesPath}".`,
-      );
-    }
-    console.log(`Found extracted app.asar at "${asarPath}"`);
-
-    await setPermissions(asarPath, 0o755);
-
-    console.log(`Update to version ${latestVersion} completed.`);
-
-    await dialog.showMessageBox({
-      type: 'info',
-      buttons: ['Restart Now'],
-      title: 'Update Ready',
-      message:
-        'The update has been installed. Please restart the app to apply changes.',
-    });
-    app.relaunch();
-    app.exit();
-  } catch (error) {
-    log.error('Error updating ASAR file:', error);
-  }
 }
 
 app.whenReady().then(async () => {
@@ -382,6 +223,47 @@ app.on('window-all-closed', () => {
 app.on('activate', () => {
   if (!mainWindow) {
     createWindow();
+  }
+});
+
+// Show dialog when an update is available
+autoUpdater.on('update-available', () => {
+  const response = dialog.showMessageBoxSync({
+    type: 'info',
+    buttons: ['Download', 'Later'],
+    title: 'Update Available',
+    message:
+      'A new version of the application is available. Would you like to download it now?',
+  });
+
+  // If the user selects "Download", initiate the update download
+  if (response === 0) {
+    // "Download" button index is 0
+    autoUpdater.downloadUpdate();
+  }
+});
+
+// Inform user when there’s a download progress
+autoUpdater.on('download-progress', (progress) => {
+  const { percent } = progress;
+  log.info(`Download progress: ${percent}%`);
+  mainWindow?.webContents.send('update-progress', percent); // Optionally send to renderer for visual progress
+});
+
+// Show dialog when update is downloaded and ready to install
+autoUpdater.on('update-downloaded', () => {
+  const response = dialog.showMessageBoxSync({
+    type: 'info',
+    buttons: ['Restart Now', 'Later'],
+    title: 'Update Ready',
+    message:
+      'The latest version has been downloaded. Do you want to restart the application to apply the update?',
+  });
+
+  // Restart the app if the user chose "Restart Now"
+  if (response === 0) {
+    // "Restart Now" button index is 0
+    autoUpdater.quitAndInstall();
   }
 });
 
